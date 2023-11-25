438f4bf4ec6a9454cf14938170e08d04d703a73b
sharma-lokesh <lokeshsharmalive@hotmail.com>
2023-11-25 12:25:39 +0530

    string learnings

--- src/main/java/geeks/strings/isPallindrome.java
+++ src/main/java/geeks/strings/isPallindrome.java
@@ -2,13 +2,26 @@ package main.java.geeks.strings;
 
 public class isPallindrome {
     public static void main(String[] args) {
-        String s = "ABCBA";
-        StringBuilder sb = new StringBuilder(s);
+        String s = "ABCCABA";
+        /*StringBuilder sb = new StringBuilder(s);
         sb.reverse();
         if(s.equals(sb.toString())){
             System.out.println("true");
         }else{
             System.out.println("false");
+        }*/
+        int begin = 0;
+        int end=s.length()-1;
+        boolean isPall = true;
+        while(end>begin){
+            if(s.charAt(begin)!=s.charAt(end)){
+                isPall = false;
+                break;
+            }else{
+                end--;
+                begin++;
+            }
         }
+        System.out.println(isPall);
     }
 }
